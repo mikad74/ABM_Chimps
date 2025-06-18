21926b8c36d0606ccc9b0e43c92ffe464529dbbb
Mika <mika.doorenbosch@gmail.com>
2025-06-18 12:42:00 +0200

    temp shit

--- animation.py
+++ animation.py
@@ -2,8 +2,14 @@ from Base_model import *
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-n_sim = 500
-model = Model(5, 20, 20)
+n_sim = 100
+model = Model(20, 20, 50)
+# model.add_oasis((2,2))
+# model.add_chimp_crew((0,1))
+# model.add_chimp_crew((1,0))
+# model.add_chimp_crew((1,2))
+# model.add_chimp_crew((2,1))
+# model.grid = model.create_grid()
 fig, ax = plt.subplots()
 img = ax.imshow(model.grid)
 
@@ -14,7 +20,9 @@ def update_ani(frame):
     return [img]
 
 ani = animation.FuncAnimation(fig, update_ani, frames=n_sim, interval=1, blit=False)
-ani.save("chimp_simulation.gif", fps=10)
+ani.save("chimp_simulation.mp4", fps=10)
 
 for oasis in model.oases.values():
     print(vars(oasis))
+for crew in model.crews.values():
+    print(vars(crew))
